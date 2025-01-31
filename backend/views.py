74c3ec32fc8fd76f3eb4972ad05f2b56e7413bec
Sergey Igumnov <jkeevk@yandex.ru>
2025-01-31 03:10:06 +0300

    Redefined update and destroy methods

--- backend/views.py
+++ backend/views.py
@@ -1,23 +1,71 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Product, ProductInfo, ProductParameter, Parameter
+from .serializers import ProductSerializer, ProductInfoSerializer, ProductParameterSerializer, ParameterSerializer
+from rest_framework.parsers import JSONParser
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.viewsets import ModelViewSet
-from .models import Product
-from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    parser_classes = [JSONParser]
+
     filter_backends = [
         DjangoFilterBackend,
         SearchFilter,
     ]
     filterset_fields = [
         "name",
+        "category",
     ]
     search_fields = [
         "name",
-        "category",
+        "category__name",
+        "model",
     ]
     pagination_class = LimitOffsetPagination
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        product_id = instance.id
+        self.perform_destroy(instance)
+
+        return Response(
+            {
+                "id": product_id,
+                "status": "success",
+                "message": f"Товар с ID {product_id} успешно удален."
+            },
+            status=status.HTTP_200_OK
+        )
+
+
+class ProductInfoViewSet(ModelViewSet):
+    queryset = ProductInfo.objects.all()
+    serializer_class = ProductInfoSerializer
+
+
+class ParameterViewSet(ModelViewSet):
+    queryset = Parameter.objects.all()
+    serializer_class = ParameterSerializer
+
+
+class ProductParameterViewSet(ModelViewSet):
+    queryset = ProductParameter.objects.all()
+    serializer_class = ProductParameterSerializer
