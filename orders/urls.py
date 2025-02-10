89ae61a451da626855fe9eb0d7b22767250b59ee
Sergey Igumnov <sergeyigumnov@MacBook-Air-Sergey.local>
2025-02-10 14:19:47 +0300

    Edited urls for Users

--- orders/urls.py
+++ orders/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for orders project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -22,25 +6,23 @@ from rest_framework_nested.routers import NestedDefaultRouter
 from rest_framework.routers import DefaultRouter
 from backend.views import (
     ProductViewSet, 
-    ShopViewSet, 
+    UserViewSet, 
     CategoryViewSet, 
-    UserDetailViewSet, 
-    ContactViewSet, 
-    RegisterAccount
-    )
+    ShopViewSet, 
+    ContactViewSet,
+)
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
-router.register(r'shops', ShopViewSet, basename='shop')
 router.register(r'categories', CategoryViewSet, basename='category')
-router.register(r'users', UserDetailViewSet, basename='user-contact')
+router.register(r'users', UserViewSet)
+router.register(r'shops', ShopViewSet)
 
 user_router = NestedDefaultRouter(router, r'users', lookup='user')
 user_router.register(r'contacts', ContactViewSet, basename='user-contacts')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/register/', RegisterAccount.as_view(), name='user-register'),
     path('api/', include(router.urls)),
     path('api/', include(user_router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
