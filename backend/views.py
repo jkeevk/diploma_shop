afb42fa4fb11787bc51586454617123b92a061fa
jkeevk <jkeevk@yandex.ru>
2025-02-13 00:10:12 +0300

    Send mail added

--- backend/views.py
+++ backend/views.py
@@ -6,11 +6,14 @@ from .serializers import (
     CategorySerializer,
     UserSerializer,
     ContactSerializer,
+    OrderSendMailSerializer,
 )
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.core.mail import send_mail
+from rest_framework.views import APIView
+from orders.settings import EMAIL_HOST_USER
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -120,3 +123,29 @@ class UserViewSet(ModelViewSet):
                 status=status.HTTP_201_CREATED,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class OrderSendMailView(APIView):
+    def post(self, request):
+        serializer = OrderSendMailSerializer(data=request.data)
+        if serializer.is_valid():
+            user_email = serializer.validated_data['user_email']
+            user_name = serializer.validated_data['user_name']
+            order_details = serializer.validated_data['order_details']
+
+            print(f"Отправка письма на: {user_email} от {user_name} с деталями: {order_details}")  # Для отладки
+
+            try:
+                send_mail(
+                    "Subject here",
+                    f"Here is the message from {user_name}. Details: {order_details}",
+                    EMAIL_HOST_USER,
+                    [user_email],
+                    fail_silently=False,
+                )
+                return Response({'status': 'Письмо отправлено'})
+            except Exception as e:
+                print(f"Ошибка при отправке письма: {e}")
+                return Response({'error': str(e)}, status=400)
+
+        return Response(serializer.errors, status=400)
