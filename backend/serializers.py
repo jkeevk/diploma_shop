3c9c2a691d9370406b628059e7e8d2d5f899ea59
jkeevk <jkeevk@yandex.ru>
2025-02-18 16:52:27 +0300

    Product CRUD reworked

--- backend/serializers.py
+++ backend/serializers.py
@@ -21,22 +21,25 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 
+class ShopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shop
+        fields = ["id", "name"]
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = "__all__"
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
 
 class ParameterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Parameter
         fields = ["name"]
-
-
-class ProductParameterSerializer(serializers.ModelSerializer):
-    parameter = serializers.SlugRelatedField(
-        slug_field="name", queryset=Parameter.objects.all()
-    )
-
-    class Meta:
-        model = ProductParameter
-        fields = ["parameter", "value"]
-
 
 class ProductInfoSerializer(serializers.ModelSerializer):
     parameters = serializers.SerializerMethodField()
@@ -50,32 +53,93 @@ class ProductInfoSerializer(serializers.ModelSerializer):
         parameters = obj.product_parameters.all()
         return {param.parameter.name: param.value for param in parameters}
 
+class ProductParameterSerializer(serializers.ModelSerializer):
+    parameter = serializers.SlugRelatedField(
+        slug_field="name", queryset=Parameter.objects.all()
+    )
+
+    class Meta:
+        model = ProductParameter
+        fields = ["parameter", "value"]
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = serializers.StringRelatedField()
+    category = CategorySerializer()
     product_infos = ProductInfoSerializer(many=True)
 
     class Meta:
         model = Product
         fields = ["id", "name", "model", "category", "product_infos"]
 
+    def create(self, validated_data):
+        category_data = validated_data.pop('category')
+        product_infos_data = validated_data.pop('product_infos')
 
-class ShopSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Shop
-        fields = ["id", "name"]
+        category, created = Category.objects.get_or_create(**category_data)
+        product = Product.objects.create(category=category, **validated_data)
 
+        for product_info_data in product_infos_data:
+            parameters_data = product_info_data.pop('parameters', [])
+            shop_data = product_info_data.get('shop')
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ["id", "name"]
+            product_info, created = ProductInfo.objects.get_or_create(
+                product=product,
+                shop=shop_data,
+                defaults=product_info_data
+            )
 
+            for param_data in parameters_data:
+                parameter_name = param_data.get('parameter')
+                value = param_data.get('value')
+                parameter, _ = Parameter.objects.get_or_create(name=parameter_name)
+                ProductParameter.objects.update_or_create(
+                    product_info=product_info,
+                    parameter=parameter,
+                    defaults={'value': value}
+                )
 
-class ContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contact
-        fields = "__all__"
+        return product
+
+    def update(self, instance, validated_data):
+        category_data = validated_data.pop('category', None)
+        product_infos_data = validated_data.pop('product_infos', None)
+
+        if category_data:
+            category, created = Category.objects.get_or_create(**category_data)
+            instance.category = category
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.model = validated_data.get('model', instance.model)
+        instance.save()
+
+        if product_infos_data is not None:
+            for product_info_data in product_infos_data:
+                parameters_data = product_info_data.pop('parameters', [])
+                shop_data = product_info_data.get('shop')
+
+                product_info_id = product_info_data.get('id', None)
+                if product_info_id:
+                    product_info = ProductInfo.objects.get(id=product_info_id, product=instance)
+                    for attr, value in product_info_data.items():
+                        setattr(product_info, attr, value)
+                    product_info.save()
+                else:
+                    product_info, _ = ProductInfo.objects.get_or_create(
+                        product=instance,
+                        shop=shop_data,
+                        defaults=product_info_data
+                    )
+
+                for param_data in parameters_data:
+                    parameter_name = param_data.get('parameter')
+                    value = param_data.get('value')
+                    parameter, _ = Parameter.objects.get_or_create(name=parameter_name)
+                    ProductParameter.objects.update_or_create(
+                        product_info=product_info,
+                        parameter=parameter,
+                        defaults={'value': value}
+                    )
+
+        return instance
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
