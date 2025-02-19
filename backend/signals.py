3ddcd07b77bd0f4d7285ec1c1cbf9ed7dda6497e
jkeevk <jkeevk@yandex.ru>
2025-02-19 11:00:19 +0300

    Sending mail messages for customers and suppliers added

--- backend/signals.py
+++ backend/signals.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.db.models.signals import post_save
 from django.conf import settings
 import uuid
-from .models import User
+from .models import User, Shop, Order
 
 @receiver(post_save, sender=User)
 def send_confirmation_email(sender, instance, created, **kwargs):
@@ -34,3 +34,48 @@ def send_password_reset_email(sender, instance, **kwargs):
             recipient_list=[instance.email],
         )
         
+@receiver(post_save, sender=Order)
+def send_order_confirmation_email(sender, instance, created, **kwargs):
+    if not created and instance.status == "confirmed":
+        shop = Shop.objects.filter(user=instance.user).first()
+        if shop:
+            host_email = shop.user.email
+            send_email_to_host(host_email, instance)
+
+        contact = instance.user.contacts.first()
+        send_email_to_customer(instance.user.email, instance, contact)
+
+def send_email_to_host(recipient_email, order, contact):
+    subject = "Поступил новый заказ"
+    message = f"Заказ #{order.id} был подтвержден.\nПодробности:\n"
+    for item in order.order_items.all():
+        message += f"Продукт: {item.product.name}\nМагазин: {item.shop.name}\nКоличество: {item.quantity}\n\n"
+    
+    if contact:
+        message += f"\nКонтактные данные:\n"
+        message += f"Город: {contact.city}\n"
+        message += f"Улица: {contact.street}\n"
+        message += f"Дом: {contact.house}\n"
+        message += f"Корпус: {contact.structure}\n"
+        message += f"Строение: {contact.building}\n"
+        message += f"Квартира: {contact.apartment}\n"
+        message += f"Телефон: {contact.phone}\n"
+    
+    send_mail(subject, message, settings.EMAIL_HOST_USER, [recipient_email])
+def send_email_to_customer(recipient_email, order, contact):
+    subject = "Ваш заказ подтвержден"
+    message = f"Ваш заказ #{order.id} был подтвержден.\nПодробности:\n"
+    for item in order.order_items.all():
+        message += f"Продукт: {item.product.name}\nМагазин: {item.shop.name}\nКоличество: {item.quantity}\n\n"
+    
+    if contact:
+        message += f"\nКонтактные данные:\n"
+        message += f"Город: {contact.city}\n"
+        message += f"Улица: {contact.street}\n"
+        message += f"Дом: {contact.house}\n"
+        message += f"Корпус: {contact.structure}\n"
+        message += f"Строение: {contact.building}\n"
+        message += f"Квартира: {contact.apartment}\n"
+        message += f"Телефон: {contact.phone}\n"
+    
+    send_mail(subject, message, settings.EMAIL_HOST_USER, [recipient_email])
