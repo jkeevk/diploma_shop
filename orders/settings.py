c6fd5b7f26f2f569d28caf1887ea512680e9c7f3
jkeevk <jkeevk@yandex.ru>
2025-04-05 00:46:52 +0300

    Add media settings

--- orders/settings.py
+++ orders/settings.py
@@ -233,7 +233,7 @@ USE_TZ = True
 STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # ==============================================================================
 # Email
